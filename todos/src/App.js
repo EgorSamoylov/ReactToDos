3c59b0382e2f42824445394ea144ef30e194356d
Egor Samoylov <mr.egoras@mail.ru>
2024-07-30 22:17:49 +0300

    Prepare application by the video about React on 1:30:00

--- todos/src/App.js
+++ todos/src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useMemo } from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import './Styles/App.css';
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript 4', body: 'Descryption'},
+    {id: 2, title: 'JavaScript 2', body: 'Descryption'},
+    {id: 3, title: 'JavaScript 3', body: 'Descryption'}
+  ]);
+
+  const [filter, setFilter] = useState({sort: '', query: ''});
+  const [modal, setModal] = useState(false);
+
+  const sortedPosts = useMemo(() => {
+    console.log('Функция отработала');
+    if(filter.sort) {
+      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+    }
+    return posts
+  }, [filter.sort, posts]);
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
+  }, [filter.query, sortedPosts]);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+    setModal(false);
+  }
+
+  // получаем post из дочернего компонента
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyButton style={{marginTop: 30}}  onClick={() => setModal(true)}>
+        Создать пользователя
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost}/>
+      </MyModal>
+      {/* Сортировка постов */}
+      <hr style={{margin: '15px 0'}}/>
+      <PostFilter 
+        filter={filter} 
+        setFilter={setFilter}
+      />
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Посты про JS'}/>
     </div>
   );
 }
