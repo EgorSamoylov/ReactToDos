8ab81eaf709e8546b00b386f4f184650dcbea9be
Egor Samoylov <mr.egoras@mail.ru>
2024-08-01 21:35:43 +0300

    Add axios (oppportunity to use get and post)

--- todos/src/App.js
+++ todos/src/App.js
@@ -23,6 +23,12 @@ function App() {
     setPosts([...posts, newPost]);
     setModal(false);
   }
+ 
+  async function fetchPosts() {
+    // get-запрос для получения данных
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    console.log(response.data);
+  }
 
   // получаем post из дочернего компонента
   const removePost = (post) => {
@@ -31,6 +37,7 @@ function App() {
 
   return (
     <div className="App">
+      <button onClick={fetchPosts}>GET POSTS</button>
       <MyButton style={{marginTop: 30}}  onClick={() => setModal(true)}>
         Создать пользователя
       </MyButton>
